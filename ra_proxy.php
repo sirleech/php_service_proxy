fbfa2fb4297f0acf15a2331739625a38d69bf13f
chris <sirleech@gmail.com>
2012-03-13 13:32:10 +1100

    use file_get_contents() instead of curl_init

--- ra_proxy.php
+++ ra_proxy.php
@@ -7,15 +7,8 @@ header('Cache-Control: no-cache');
 header('Pragma: no-cache');
 header("Access-Control-Allow-Origin: *");
 
-$ch = curl_init("http://ausbots.com.au/json_php/ra.php");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-$json = curl_exec($ch);
-curl_close($ch);
+$url = 'http://ausbots.com.au/json_php/ra.php';
 
-$codec = new Services_JSON();
-$searchValues = $codec->decode($json);
-
-echo $json;
+echo file_get_contents($url);
 
 ?>
